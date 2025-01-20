44571e22784ff8f5ba696e05d68ac2e34e220473
Joao Labussiere <labussework@gmail.com>
2025-01-20 03:29:48 -0300

    add function minutos_para_horas()

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from models.biblioteca import Livros
 
-livro1 = Livros('George Orwell', 'A Revolução dos Bichos', 2025, 224)
-
+livro1 = Livros('George Orwell', 'A Revolução dos Bichos', 2025)
+livro1.adicionar_tempo_de_leitura(224)
 livro1.alternar_status()
-livro2 = Livros('George Orwell', '1984', 2025, 0)
+
+livro2 = Livros('George Orwell', '1984', 2025)
 
 def __main__():
     Livros.lista_livros()
--- models/biblioteca.py
+++ models/biblioteca.py
@@ -3,18 +3,17 @@ class Livros:
 
     livros = []
 
-    def __init__(self, autor: str, titulo: str, ano_da_leitura: int, tempo_de_leitura_em_minutos: int):
-        if tempo_de_leitura_em_minutos < 0:
-            raise ValueError("O tempo de leitura deve ser maior ou igual a 0!")
+    def __init__(self, autor: str, titulo: str, ano_da_leitura: int):
         if len(str(ano_da_leitura)) != 4:
             raise ValueError("O ano da leitura deve conter 4 dígitos!")
-        elif ano_da_leitura > 2025:
+        
+        if ano_da_leitura > 2025:
             raise ValueError("O ano da leitura deve ser menor ou igual a 2025!")
 
         self.autor = autor.title()
         self.titulo = titulo.title()
         self.ano_da_leitura = ano_da_leitura
-        self.tempo_de_leitura_em_minutos = tempo_de_leitura_em_minutos
+        self.tempo_de_leitura_em_minutos = 0
         self.status = False
         Livros.livros.append(self)
 
@@ -26,11 +25,12 @@ class Livros:
         os.system('cls')
         if not Livros.livros:
             print('Nenhum livro foi adicionado à lista. Adicione um livro para visualizar a lista!')
-        else:
-            print('\n-----------------------------------| Lista de Livros Lidos em 2025 |----------------------------------')
-            print(f'\n| {'Autor:':<30} | {'Título:':<30} | {'Status:':<30} | {'Tempo de Leitura:':<30} |')
-            for livro in cls.livros:
-                print(f'| {livro.autor:<30} | {livro.titulo:<30} | {livro.definir_status():<30} | {livro.minutos_para_horas():<30} |')
+            return
+        
+        print('\n-----------------------------------| Lista de Livros Lidos em 2025 |----------------------------------')
+        print(f'\n| {'Autor:':<20} | {'Título:':<25} | {'Status:':<22} | {'Tempo de Leitura:':<22} |')
+        for livro in cls.livros:
+            print(f'| {livro.autor:<20} | {livro.titulo:<25} | {livro.definir_status():<22} | {livro.minutos_para_horas():<22} |')
     
     def alternar_status(self):
         self.status = not self.status
@@ -39,7 +39,13 @@ class Livros:
         return 'Leitura Finalizada' if self.status else 'Leitura em Andamento'
 
     def minutos_para_horas(self):
-        horas = self.tempo_de_leitura_em_minutos // 60
-        minutos = self.tempo_de_leitura_em_minutos % 60
-        return f'{horas} horas e {minutos} minutos'
+        horas, minutos = divmod(self.tempo_de_leitura_em_minutos, 60)
+        hora_str = "hora" if horas == 1 else "horas"
+        minuto_str = "minuto" if minutos == 1 else "minutos"
+        return f'{horas} {hora_str} e {minutos} {minuto_str}' if horas or minutos else '0 hora e 0 minuto'
+    
+    def adicionar_tempo_de_leitura(self, minutos: int):
+        self.tempo_de_leitura_em_minutos += minutos
+
+    
 
